df8f821418683a4b58e0670a030f0114b642c040
delchev <delchevn@gmail.com>
2022-09-16 11:51:25 +0300

    caption added

--- template-application-ui-angular/ui/perspective/master-list/tile.js
+++ template-application-ui-angular/ui/perspective/master-list/tile.js
@@ -2,6 +2,8 @@ exports.getTile = function () {
     return {
         group: "{{perspectiveName}}",
         name: "{{name}}",
+        caption: "{{caption}}",
+        tooltip: "{{tooltip}}",
         // icon: "file-o",
         location: "/services/v4/web/{{projectName}}/gen/ui/{{perspectiveName}}/index.html",
         order: "100"
